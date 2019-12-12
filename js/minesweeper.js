48876bbb0232b5d330df4a0ff3dbfeadeab29220
Nathaniel McDonald <muted.pizza@hotmail.com>
2019-12-12 15:47:43 -0800

    Added options for number of mines when game is started

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,3 +1,24 @@
-$("#GameRules").click(function() {
-  document.location.href = 'https://en.wikipedia.org/wiki/Minesweeper_(video_game)'
+let gameSizes = []
+let difficulties = [10, 40, 99];
+
+$("#StartGame").click(function() {
+  $("#StartGame").toggleClass("hidden");
+  $("#ResetFlagButton").toggleClass("hidden");
+  $("#ResetGameButton").toggleClass("hidden");
+  let gameSection = $("#GameSection");
+  let mineCountButtons = "<div class='container-flex'><h2>Please select number of mines:</h2></div>";
+  for(let i = 0; i < difficulties.length; i++) {
+    mineCountButtons += "<button id='" + difficulties[i] + "' class='Button'>" + difficulties[i] + "</button>";
+  };
+  gameSection.html(mineCountButtons);
+  $("#GameSection .Button").click(function() {
+    mines = $(this).attr("id");
+    console.log(mines);
+  });
 });
+
+let mines = 0;
+
+
+
+let gameBoard = [];
