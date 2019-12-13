4b4aebfa5a3f199393cc59b961617f4d7b5b01ff
Nathaniel McDonald <muted.pizza@hotmail.com>
2019-12-12 17:43:14 -0800

    Added functions to generate the gameBoard

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,12 +1,32 @@
-let gameSizes = []
-let difficulties = [10, 40, 99];
+// gameSizes[i] is the dimensions for the gameBoard corresponding
+// to difficulties[i], difficulties[i] is the number of mines on the gameBoard
+let gameSizes = [[9,9],[16,16],[16,30]]
+let difficulties = ["10", "40", "99"];
 let gameBoard = [];
 let mines = 0;
 
+function createGameBoard(index) {
+  for(let i = 0; i < gameSizes[index][0]; i++) {
+    gameBoard[i] = [];
+    for(let j = 0; j < gameSizes[index][1]; j++) {
+      gameBoard[i][j] = null;
+    };
+  };
+  console.log(gameBoard);
+};
+
 function generateGame() {
+  for(let i = 0; i < difficulties.length; i++) {
+    if(mines == difficulties[i]) {
+      createGameBoard(i);
+      break;
+    };
+    // TODO create table to display board
+  };
   return "Game in progress.";
 };
 
+// These buttons are hidden initially and when choosing the number of mines
 function toggleGameButtons() {
   $("#NewGame").toggleClass("hidden");
   $("#ResetFlagButton").toggleClass("hidden");
@@ -41,4 +61,5 @@ $("#StartGame").click(function() {
 $("#NewGame").click(function() {
   toggleGameButtons();
   displayMineCountButtons();
+  gameBoard = [];
 });
