50aec22f3c7cd2f8e10bdcab1b21d466712a04ae
Nathaniel McDonald <muted.pizza@hotmail.com>
2019-12-12 16:02:45 -0800

    Added new game button

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,24 +1,35 @@
 let gameSizes = []
 let difficulties = [10, 40, 99];
+let gameBoard = [];
+let mines = 0;
 
-$("#StartGame").click(function() {
-  $("#StartGame").toggleClass("hidden");
-  $("#ResetFlagButton").toggleClass("hidden");
-  $("#ResetGameButton").toggleClass("hidden");
+function generateGame() {
+  return "Game in progress.";
+};
+
+function displayMineCountButtons() {
   let gameSection = $("#GameSection");
   let mineCountButtons = "<div class='container-flex'><h2>Please select number of mines:</h2></div>";
   for(let i = 0; i < difficulties.length; i++) {
     mineCountButtons += "<button id='" + difficulties[i] + "' class='Button'>" + difficulties[i] + "</button>";
   };
   gameSection.html(mineCountButtons);
+
   $("#GameSection .Button").click(function() {
     mines = $(this).attr("id");
+    gameSection.html(generateGame());
     console.log(mines);
   });
+};
+
+$("#StartGame").click(function() {
+  $("#StartGame").toggleClass("hidden");
+  $("#NewGame").toggleClass("hidden");
+  $("#ResetFlagButton").toggleClass("hidden");
+  $("#ResetGameButton").toggleClass("hidden");
+  displayMineCountButtons();
 });
 
-let mines = 0;
-
-
-
-let gameBoard = [];
+$("#NewGame").click(function() {
+  displayMineCountButtons();
+});
